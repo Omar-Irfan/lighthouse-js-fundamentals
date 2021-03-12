eed69aef2c81f94e5134abb198b7eab5094ae9e5
Omar Irfan <omar.irfan18@gmail.com>
2021-03-12 23:17:50 +0000

    function practice

---
+++ hello.js
@@ -0,0 +1,17 @@
+const sayBye  = function () {
+  console.log("Goodbye, world");
+}
+
+sayBye();
+sayBye();
+sayBye();
+sayBye();
+sayBye();
+
+const sayHello = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHello("Frodo");
+sayHello("Sam");
+sayHello("Gandalf");
